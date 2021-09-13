6713a17cbc05d61f06f284ed5ad30899f8ac0199
IneseKo <in.kocina#gmail.com>
2021-09-13 15:06:20 +0300

    Completed the 2nd task "Time"

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Time {
     public static void main(String[] args) {
-
+        int time = 20;
+if (time>=5 && time<=12)
+    System.out.println("Good Morning, Sunshine!");
+else if (time>=13 && time<=19)
+    System.out.println("Good Afternoon! Work hard!");
+else if (time>=20 && time<=24)
+    System.out.println("Good Evening! Get some rest!");
     }
 }
